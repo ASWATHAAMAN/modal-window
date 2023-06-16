31a5886202ec641e6f63255edd80d96be58c2c04
ASWATHAAMAN A C <aswathaaman19@gmail.com>
2023-06-16 22:48:12 +0530

    started

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,20 @@
 `use strict`;
+
+// select elements
+
+const buttonOneEl = document.getElementById(`btn-one`);
+const buttonTwoEl = document.getElementById(`btn-two`);
+const buttonThreeEl = document.getElementById(`btn-three`);
+const closeButtonEl = document.getElementById(`close-btn`);
+const modalContainerEl = document.getElementById(`modal-container`);
+// global variables
+
+// functions
+ const init = function(){
+  modalContainerEl.classList.add(`close`);
+ };
+  
+// event listeners
+
+// initial settings
+init();
